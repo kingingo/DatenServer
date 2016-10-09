943c4b2120b64f0b26dd77b10ebd060b2f130bbb
Janmm14 <computerjanimaus@yahoo.de>
2016-10-09 02:08:39 +0200

    Change ts skin download method to be more robust

--- src/eu/epicpvp/teamspeak/MinecraftUtils.java
+++ src/eu/epicpvp/teamspeak/MinecraftUtils.java
@@ -7,21 +7,24 @@ import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
-
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+import javax.swing.WindowConstants;
 
 import eu.epicpvp.datenserver.definitions.hashmaps.CachedHashMap;
+import lombok.NonNull;
 
 public class MinecraftUtils {
 	private static final Rectangle HEAD_REGION = new Rectangle(8, 8, 8, 8);
 	private static final Rectangle HEAD_OVERLAY_REGION = new Rectangle(5*8, 8, 8, 8);
-	private static CachedHashMap<String, BufferedImage> skins = new CachedHashMap<>(1, TimeUnit.MINUTES);
+	private static CachedHashMap<String, BufferedImage> skins = new CachedHashMap<>(10, TimeUnit.MINUTES);
 
 	public static BufferedImage getSkin(String name) {
+		name = name.toLowerCase().intern();
 		if (skins.containsKey(name) && skins.get(name) != null)
 			return skins.get(name);
 
@@ -29,17 +32,27 @@ public class MinecraftUtils {
 		try {
 			url = new URL("http://skins.minecraft.net/MinecraftSkins/" + name + ".png");
 		} catch (MalformedURLException e) {
-			System.out.println("Unable to parse skin URL");
+			System.err.println("Unable to parse skin URL");
+			e.printStackTrace();
+			return null;
 		}
 
 		try {
-			InputStream inStream = url.openStream();
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setInstanceFollowRedirects(true);
+			conn.setDoInput(true);
+			conn.setDoOutput(false);
+			conn.setRequestMethod("GET");
+			conn.setDefaultUseCaches(false);
+			conn.setUseCaches(false);
+			InputStream inStream = conn.getInputStream();
 			BufferedImage image = ImageIO.read(inStream);
 			skins.put(name, image);
 			inStream.close();
 			return image;
 		} catch (IOException e) {
-			System.out.println("IOException while downloading skin!");
+			System.err.println("IOException while downloading skin!");
+			e.printStackTrace();
 		}
 		return null;
 	}
@@ -48,6 +61,11 @@ public class MinecraftUtils {
 		BufferedImage skin = getSkin(name);
 		if(skin == null)
 			return null;
+		return getHeadPart(skin, overlay);
+	}
+	
+	@NonNull
+	public static BufferedImage getHeadPart(BufferedImage skin, boolean overlay) {
 		BufferedImage image = new BufferedImage(8, 8, BufferedImage.TYPE_INT_ARGB);
 		Graphics2D grafics = (Graphics2D) image.getGraphics();
 		grafics.setColor(Color.RED);
@@ -62,11 +80,11 @@ public class MinecraftUtils {
 		grafics.dispose();
 		return image;
 	}
-
+	
 	public static void main(String[] args) {
 		JFrame frame = new JFrame() {
 			public void paint(java.awt.Graphics g) {
-				((Graphics2D)g).scale(5, 5);
+				((Graphics2D)g).scale(1, 1);
 				
 				/*
 				BufferedImage image = getSkin("WolverinDEV");
@@ -80,11 +98,13 @@ public class MinecraftUtils {
 				((Graphics2D)g).drawRect((int) HEAD_OVERLAY_REGION.getX(), (int) HEAD_OVERLAY_REGION.getY(), (int) HEAD_OVERLAY_REGION.getWidth(),(int) HEAD_OVERLAY_REGION.getHeight());
 				*/
 				Image img = getHead("Janmm14", true);
-				img = img.getScaledInstance(16, 16, 1);
-				g.drawImage(img, 0, 0, this);
+				img = img.getScaledInstance(16 * 8, 16 * 8, 1);
+				g.drawImage(img, 100 - 8 * 8, 100 - 8 * 8, this);
 			};
 		};
-		frame.setBounds(100, 100, 1000, 1000);
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.setSize(200, 200);
+		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 	}
 
