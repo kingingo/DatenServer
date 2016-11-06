6c0e0296388c3587b5b996b3ac7b40c172f55855
Janmm14 <computerjanimaus@yahoo.de>
2016-11-06 19:45:39 +0100

    Only put the simple class name of the logging class to the console output

--- src/eu/epicpvp/dataserver/terminal/Debugger.java
+++ src/eu/epicpvp/dataserver/terminal/Debugger.java
@@ -4,13 +4,16 @@ public class Debugger {
 	public static void printMessage(String message){
 		System.out.println("[Debugger] ["+getLastCallerClass()+"] -> "+message);
 	}
-	
+
 	public static String getLastCallerClass(){
 		StackTraceElement[] stack = Thread.currentThread().getStackTrace();
-		for (int i = 0; i < stack.length; i++) {
-			StackTraceElement currunt = stack[i];
-			if(!currunt.getClassName().equalsIgnoreCase(Debugger.class.getName()) && !currunt.getClassName().equalsIgnoreCase("java.lang.Thread") && !currunt.getClassName().contains("eu.epicpvp.dataserver.terminal.")){
-				return currunt.getClassName()+":"+currunt.getLineNumber();
+		for (StackTraceElement currunt : stack) {
+			if (!currunt.getClassName().equalsIgnoreCase(Debugger.class.getName()) && !currunt.getClassName().equalsIgnoreCase("java.lang.Thread") && !currunt.getClassName().contains("eu.epicpvp.dataserver.terminal.")) {
+				try {
+					return Class.forName(currunt.getClassName()).getSimpleName() + ":" + currunt.getLineNumber();
+				} catch (ClassNotFoundException e) {
+					return currunt.getClassName() + ":" + currunt.getLineNumber();
+				}
 			}
 		}
 		return "unknown:-1";
